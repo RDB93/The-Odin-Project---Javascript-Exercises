7b1118b540b29c5fedd6a9ddc2c2bbbb81bfc1b6
Ryan Dean Brink <RDBProgram@gmail.com>
2018-04-18 09:54:56 +0200

    Finished tempConversion exercise, started the sumAll exercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,11 @@
-var sumAll = function() {
-
+var sumAll = function(x,y) {
+  var list = [];
+  for (var i = x; i <= y; i++) {list.push(i);}
+  var sum = list.reduce((acc, list) => acc + list);
+  return(sum);
 }
 
 module.exports = sumAll
+
+
+/*problem when x and y are swapped throwing errors
--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,10 +1,13 @@
-var ftoc = function() {
-  
-}
+var ftoc = function(fahrenheit) {
+  var celsius = ((fahrenheit -32)*5/9);
+  return(Math.round(celsius*10)/10);
+  }
+  	
 
-var ctof = function() {
-  
-}
+var ctof = function(celsius) {
+  var fahrenheit = ((celsius * 9)/5 + 32);
+  return(Math.round(fahrenheit*10)/10);
+} 
 
 module.exports = {
   ftoc,
