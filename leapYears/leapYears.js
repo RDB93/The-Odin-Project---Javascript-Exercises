219fcdb59fd729fc3639c89cd2326efee3416b3f
Ryan Dean Brink <RDBProgram@gmail.com>
2018-05-10 17:23:09 +0200

    Finished the leapYears exercise

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,12 @@
-var leapYears = function() {
+var leapYears = function(leapYears) {
+	if (leapYears % 100 === 0 && leapYears % 400 !== 0) {
+		return(false);
+	} else if (leapYears % 4 === 0) {
+		return(true);
+	} else {
+		return(false);
+		}
+	}
 
-}
 
 module.exports = leapYears
